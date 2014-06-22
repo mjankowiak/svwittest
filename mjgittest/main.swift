a3fda2f9cedb060809c4a0d6cd7d2b069f1283e9
Michael Jankowiak <Jankowiak@Excelsior.local>
2014-06-22 21:05:32 +0200

    Initial Commit

---
+++ mjgittest/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  mjgittest
+//
+//  Created by Michael Jankowiak on 22.06.14.
+//  Copyright (c) 2014 Michael Jankowiak. All rights reserved.
+//
+
+import Cocoa
+
+NSApplicationMain(C_ARGC, C_ARGV)
---
+++ mjgittestTests/mjgittestTests.swift
@@ -0,0 +1,35 @@
+//
+//  mjgittestTests.swift
+//  mjgittestTests
+//
+//  Created by Michael Jankowiak on 22.06.14.
+//  Copyright (c) 2014 Michael Jankowiak. All rights reserved.
+//
+
+import XCTest
+
+class mjgittestTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
